f53bbfc93b86a025be1e912d35b90444e27cf8e7
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-12-15 00:04:08 +0100

    feat: create buttons with click handles in home component

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello, ninjas!', e);
+    }
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => {
+                handleClickAgain('mario')
+            }}>Click me again</button>
         </div>
     );
 }
