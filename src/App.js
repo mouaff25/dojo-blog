afa3bcec3fc2d037876815834df6f67d7a78fb5f
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-12-14 23:44:11 +0100

    feat: create navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   const title = 'Welcome to the new blog';
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>{title}</h1>
       </div>
