5a4a4bcf67be60f6cd83fcdd8035e5626e76d453
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-12-15 02:21:14 +0100

    feat: add age state to button

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,23 @@ import { useState } from 'react';
 
 const Home = () => {
     const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
     const handleClick = () => {
         if (name === 'mario') {
             setName('luigi');
+            setAge(30);
         }
         else {
             setName('mario');
+            setAge(25);
         }
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name }</p>
+            <p>{ name } is { age } old</p>
             <button onClick={handleClick}>Click me</button>
         </div>
     );
