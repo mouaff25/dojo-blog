b0efb591f3f7d5721225ec6ca37f581d7a651e85
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-12-15 18:22:54 +0100

    feat: add links in navbar that point to home and create page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
