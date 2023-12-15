82c57ce676f0f40efb3b26c890632daec6e3b4e0
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-12-15 18:35:17 +0100

    style: remove text decoration from blog review

--- src/BlogList.js
+++ src/BlogList.js
@@ -8,12 +8,13 @@ const BlogList = (props) => {
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map((blog) => (
-                <Link to={`/blogs/${blog.id}`} key={blog.id}>
                     <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>Written by {blog.author}</p>
+                        <Link to={`/blogs/${blog.id}`}>
+                            <h2>{blog.title}</h2>
+                            <p>Written by {blog.author}</p>
+                        </Link>
                     </div>
-                </Link>))
+                    ))
             }
         </div>
     );
