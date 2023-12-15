f8266ca541099c586465978aa6e4947a3ba8bd01
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-12-15 12:15:19 +0100

    fix: return error message for rendering instead of error object

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,22 +4,22 @@ const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    
+
     useEffect(() => {
         const fetchData = async () => {
-        try {
-            const res = await fetch(url);
-            const json = await res.json();
-            setData(json);
-            setLoading(false);
-        } catch (error) {
-            setError(error);
-            setLoading(false);
-        }
+            try {
+                const res = await fetch(url);
+                const json = await res.json();
+                setData(json);
+                setLoading(false);
+            } catch (error) {
+                setError(error.message);
+                setLoading(false);
+            }
         };
         fetchData();
     }, [url]);
-    
+
     return { data, loading, error };
 }
 
